34564fdcb4bad648fa7e9daf522c85ca30eaa255
DoggoVN <lolgamer1704@gmail.com>
2024-09-28 22:58:19 +0700

    Update aimbot1.lua

--- aimbot1.lua
+++ aimbot1.lua
@@ -101,7 +101,7 @@ getgenv().ExunysDeveloperAimbot = {
 
 		TeamCheck = true,
 		AliveCheck = true,
-		WallCheck = true,
+		WallCheck = false,
 
 		OffsetToMoveDirection = false,
 		OffsetIncrement = 15,
